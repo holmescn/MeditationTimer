7c47717ceffa3dedea30a158f3d286c2d4116dca
Holmes Conan <holmesconan@gmail.com>
2020-10-07 09:27:40 +0800

    Done UI design.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PickerController, PopoverController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  timeRemain: string = "等待设置";
 
-  constructor() {}
+  constructor(private popover: PopoverController, private picker: PickerController) {
+  }
 
+  onClick(event) {
+    console.log("onClick");
+  }
 }
