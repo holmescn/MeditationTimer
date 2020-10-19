ba428a404f9ce8180dd42d54a790021cc3cbb984
Holmes Conan <holmesconan@gmail.com>
2020-10-19 22:03:43 +0800

    1. Can select time and sound(template; 2. can play sound.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { PickerColumn } from '@ionic/core';
 import { PickerController, PopoverController } from '@ionic/angular';
 
 @Component({
@@ -7,12 +8,87 @@ import { PickerController, PopoverController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  timeRemain: string = "等待设置";
+  @ViewChild('player') playerRef: ElementRef<HTMLAudioElement>;
+  timePickerColumns: PickerColumn[] = [{
+    name: 'hours',
+    options: [...Array(99).keys()].map(i => Object.assign({
+      text: (i < 10 ? `0${i}` : `${i}`),
+      value: i
+    }))
+  }, {
+    name: 'minutes',
+    options: [...Array(99).keys()].map(i => Object.assign({
+      text: (i < 10 ? `0${i}` : `${i}`),
+      value: i
+    }))
+  }];
+  soundPickerColumns: PickerColumn[] = [{
+    name: 'sound',
+    options: [{
+      text: 'sound-1',
+      value: 'sound-1'
+    }]
+  }];
+  timeRemain    = "等待设置";
+  targetMinutes = 0;
+  targetSound   = 'sound-1';
+  state = '';
 
   constructor(private popover: PopoverController, private picker: PickerController) {
+    
+  }
+
+  get player() {
+    return this.playerRef.nativeElement;
+  }
+
+  async pick(type: string) {
+    if (type === 'time') {
+      const picker = await this.picker.create({
+        columns: this.timePickerColumns,
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel'
+          },
+          {
+            text: 'Confirm',
+            handler: (value) => {
+              this.targetMinutes = value.hours.value * 60 + value.minutes.value
+            }
+          }
+        ]
+      });
+      await picker.present();
+    } else if (type === 'sound') {
+      const picker = await this.picker.create({
+        columns: this.soundPickerColumns,
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel'
+          },
+          {
+            text: 'Confirm',
+            handler: (value) => {
+              console.log(`Got Value`, value);
+            }
+          }
+        ]
+      });
+      await picker.present();
+    }
   }
 
   onClick(event) {
-    console.log("onClick");
+    this.player.play();
+  }
+
+  formatTime(minutes: number): string {
+    const h = Math.floor(minutes / 60);
+    const m = minutes % 60;
+    const hh = h < 10 ? `0${h}` : `${h}`;
+    const mm = m < 10 ? `0${m}` : `${m}`;
+    return `${hh}:${mm}`;
   }
 }
